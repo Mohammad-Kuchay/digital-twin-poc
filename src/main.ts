09b432b7d97512e3a52edd4390f6f44da3d4e9f1
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-03-06 17:44:35 +0530

    updates readme and refactors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrapDigitalTwin() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('digital-twin-poc')
@@ -19,4 +19,9 @@ async function bootstrap() {
   });
   await app.listen(8000);
 }
-bootstrap();
+
+if (require.main === module) {
+  bootstrapDigitalTwin();
+}
+
+export { bootstrapDigitalTwin };
