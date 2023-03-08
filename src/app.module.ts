054c3ea490a6e4dcf78241abea01aa9201498277
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-03-08 10:26:56 +0530

    remove comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,33 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AirportModule } from './application/rest_api/controllers/airport/airport.module';
 import { GremlinService } from './infrastructure/data_access/gremlin/gremlin.service';
-// import { TerminalModule } from './application/rest_api/controllers/terminal/terminal.module';
-// import { RoomModule } from './application/rest_api/controllers/room/room.module';
-// import { IotDeviceModule } from './application/rest_api/controllers/iot_device/iot_device.module';
-// import { CountryModule } from './application/rest_api/controllers/country/country.module';
-// import { StateModule } from './application/rest_api/controllers/state/state.module';
-// import { CityModule } from './application/rest_api/controllers/city/city.module';
 import { EntityModule } from './application/rest_api/controllers/entity/entity.module';
-// import { SearchModule } from './application/rest_api/controllers/search/search.module';
-// import { GraphModule } from './application/rest_api/controllers/graph/graph.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    // AirportModule,
-    // TerminalModule,
-    // RoomModule,
-    // IotDeviceModule,
-    // CountryModule,
-    // StateModule,
-    // CityModule,
-    // SearchModule,
     EntityModule,
-    // GraphModule,
   ],
   controllers: [AppController],
   providers: [
